249f1922a3ce31af74c8ec3bf7915c93df273f93
hallcode <alexhall93@me.com>
2023-03-28 01:29:08 +0100

    Finished datatable

--- src/components/ptu-datatable/ptu-datatable.tsx
+++ src/components/ptu-datatable/ptu-datatable.tsx
@@ -37,23 +37,43 @@ export class PtuDatatable {
 
   setOffset(pageIndex: number) {
     this.offset = this.maxItems * pageIndex;
+    this.root.scrollIntoView()
+  }
+
+  setLimit(max) {
+    this.maxItems = max * 1;
+    this.root.scrollIntoView()
   }
 
   pages(): Array<any> {
-    const pageCount = this.root.childElementCount / this.maxItems;
+    const pageCount = this.totalPages();
     let pages = [];
-    for (let i = 0; i <= pageCount; i++) {
+    const allowedPages = [
+      0, 1, pageCount-1, pageCount-2, this.currentPageIndex(), this.currentPageIndex() - 1, this.currentPageIndex() + 1
+    ];
+    for (let i = 0; i < pageCount; i++) {
+      if (!allowedPages.includes(i)) {
+        if (pages[pages.length-1] == "...") {
+          continue
+        }
+        pages.push("...")
+        continue;
+      }
       let className = "page-button";
       if (i == this.currentPageIndex()) {
         className += " active";
       }
       pages.push((
-        <button class={className} onClick={() => this.setOffset(i)}>
+        <button id={`${this.caption}__page_button__${i}`}
+                class={className}
+                onClick={() => this.setOffset(i)}
+                aria-label={`Go to page ${i+1}`}
+                title={`Go to page ${i+1}`}
+        >
           {i+1}
         </button>
       ))
     }
-
     return pages;
   }
 
@@ -61,7 +81,23 @@ export class PtuDatatable {
     if (this.offset === 0) {
       return this.offset
     }
-    return this.maxItems / this.offset;
+    return Math.ceil(this.offset / this.maxItems);
+  }
+
+  pageBack() {
+    if (this.currentPageIndex() > 0) {
+      this.setOffset(this.currentPageIndex() - 1)
+    }
+  }
+
+  pageForward() {
+    if (this.currentPageIndex() < this.totalPages()) {
+      this.setOffset(this.currentPageIndex() + 1)
+    }
+  }
+
+  totalPages() {
+    return Math.ceil(this.root.childElementCount / this.maxItems)
   }
 
   render() {
@@ -108,8 +144,43 @@ export class PtuDatatable {
           </tbody>
         </table>
         <footer>
+          <div class="select-wrapper">
+            Display
+            {/* @ts-ignore */}
+            <select onChange={(e) => this.setLimit(e.target.value)}>
+              <option value={10}>10</option>
+              <option value={25}>25</option>
+              <option value={50}>50</option>
+            </select>
+          </div>
           <nav>
+            {this.currentPageIndex() > 0 && (
+              <button class="page-button"
+                      title={`Previous ${this.maxItems}`}
+                      aria-label={`Previous ${this.maxItems}`}
+                      onClick={() => this.pageBack()}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em">
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path fill="currentColor"
+                        d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z" />
+                </svg>
+              </button>
+            )}
             {this.pages()}
+            {this.currentPageIndex() < this.totalPages() - 1 && (
+              <button class="page-button"
+                      title={`Next ${this.maxItems}`}
+                      aria-label={`Next ${this.maxItems}`}
+                      onClick={() => this.pageForward()}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em">
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path fill="currentColor"
+                        d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" />
+                </svg>
+              </button>
+            )}
           </nav>
         </footer>
       </Host>
